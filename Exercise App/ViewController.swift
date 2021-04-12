7382fd045920fd3640bfd63fad9973245e59ae46
LimMengShin <lim_meng_shin@s2019.ssts.edu.sg>
2021-04-12 16:10:28 +0800

    Initial Commit

---
+++ Exercise App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Exercise App
+//
+//  Created by Lim Meng Shin on 12/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
